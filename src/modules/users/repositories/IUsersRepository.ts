5779515dd0cf908526f4648b78ba6a47b9996b4f
Vinicius Flores <37738541+viniciusflores@users.noreply.github.com>
2020-11-22 17:49:21 -0300

    User solid (#1)
    * :construction: start solid architecture
    
    * :construction: start create user service
    
    * :construction: start create user service v2
    
    * :construction: start create user service v3 - run linters
    
    * :construction: create users - migration
    
    * :construction: run users - migration
    
    * :whale: add docker file
    
    * :alien: include postman request
    
    * :fix: fix inject dependencies
    
    * Delete ormconfig.json
    
    * :see_no_evil: update .gitignore

---
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,7 @@
+import User from '@modules/users/infra/typeorm/entities/User';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+export default interface IUsersRepository {
+  findByEmail(email: string): Promise<User | undefined>;
+  create(data: ICreateUserDTO): Promise<User>;
+}
