0eb67242b23858a8e4235ab1b714be3b3fbc5c27
Vinicius Flores <viniciusflores379@gmail.com>
2020-11-22 17:42:49 -0300

    :fix: fix inject dependencies

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,6 +4,9 @@ import '@modules/users/providers';
 // import '@shared/container/providers'
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import UserRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-container.registerSingleton<IUsersRepository>('UserRepository', UserRepository);
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
