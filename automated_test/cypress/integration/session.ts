fe3b1e0e47363910c919b7a33e61c59bd4de827d
Vinicius Flores <viniciusflores379@gmail.com>
2020-12-03 08:55:13 -0300

    :sparkles: complete cy auth test

--- automated_test/cypress/integration/session.ts
+++ automated_test/cypress/integration/session.ts
@@ -30,4 +30,54 @@ describe('AuthenticateTest', () => {
       assert.exists(res.body.user);
     });
   });
+
+  it('Should not be able to authenticate with non existing user', () => {
+    cy.request({
+      method: 'POST',
+      url: 'sessions',
+      body: {
+        email: 'nonexistent-user@example.com',
+        password: '123456',
+      },
+      failOnStatusCode: false,
+    }).as('response');
+
+    cy.get('@response').then((res) => {
+      assert.equal(res.status, 401);
+      assert.equal(res.body.status, 'error');
+      assert.equal(res.body.message, 'Incorrect email/password combination');
+    });
+  });
+
+  it('Should not be able to authenticate with wrong password', () => {
+    const email = `johndoe${Date.now()}@email.com`;
+
+    cy.request({
+      method: 'POST',
+      url: '/users',
+      body: {
+        name: 'John Doe',
+        email,
+        password: '123456',
+      },
+    }).then((res) => {
+      assert.equal(res.status, 201);
+    });
+
+    cy.request({
+      method: 'POST',
+      url: 'sessions',
+      body: {
+        email,
+        password: 'wrong-password',
+      },
+      failOnStatusCode: false,
+    }).as('response');
+
+    cy.get('@response').then((res) => {
+      assert.equal(res.status, 401);
+      assert.equal(res.body.status, 'error');
+      assert.equal(res.body.message, 'Incorrect email/password combination');
+    });
+  });
 });
